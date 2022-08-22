0ffeeb9f27a4415928194270443f0736e3b8daf3
Jonay Brito Hernández <jonaybrito15@gmail.com>
2022-08-22 18:06:57 +0100

    Ejercicios terminados

--- js/main.js
+++ js/main.js
@@ -6,7 +6,11 @@
 
 
 function ejercicio01(email){
-    console.log(email);
+    if (email === "yunior.developer@hotmail.com" || email === "miguel@mrbug.es" || email === "imanol@mercadona.com") {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 
@@ -21,7 +25,11 @@ function ejercicio01(email){
 
 
 function ejercicio02(email){
-    console.log(email);
+    if (email.includes("m")) {
+        return `El correo ${email} tiene ${email.length} caracteres y en mayúsculas se quedaría así ${email.toUpperCase()}. Además contiene letras M`;
+    } else { 
+        return `El correo ${email} tiene ${email.length} caracteres y en mayúsculas se quedaría así ${email.toUpperCase()}. Además no contiene ninguna letras M`;
+    }
 }
 
 
@@ -36,7 +44,15 @@ function ejercicio02(email){
 
 
 function ejercicio03(email){
-    console.log(email);
+    var emailSplit = email.split("");
+    var emailSplitWithAr = email.split("@");
+    for (let index = 0; index < email.length; index++) {
+        if (isNaN(parseInt(emailSplit[index]))) {
+            return `El correo ${email} pertenece al dominio ${emailSplitWithAr[1]} y tiene ${emailSplitWithAr[0].length} caracteres sin contar el dominio ni el @. Además, el correo contiene número/s`
+        } else {
+            return `El correo ${email} pertenece al dominio ${emailSplitWithAr[1]} y tiene ${emailSplitWithAr[0].length} caracteres sin contar el dominio ni el @. Además, el correo no contiene ningún número`
+        }
+    }
 }
 
 
@@ -51,7 +67,11 @@ function ejercicio03(email){
 
 
 function ejercicio04(user){
-    console.log(user);
+    if (user.edad < 18) {
+        return `El usuario ${user.nombre} no es mayor de edad.`;
+    } else {
+        return `El usuario ${user.nombre} es mayor de edad. Por lo tanto, le he creado un usuario con el correo ${user.correo}`;
+    }
 }
 
 
@@ -66,7 +86,19 @@ function ejercicio04(user){
 
 
 function ejercicio05(user){
-    console.log(user);
+    if( user.nombre.includes("Yunior") && user.correo === "yunior.developer@hotmail.com" && parseInt(user.edad) === 27){
+        return "La persona introducida es Yunior";
+    } else if (user.nombre.includes("Yunior") || user.correo === "yunior.developer@hotmail.com" || parseInt(user.edad) === 27) {
+        if(user.nombre.includes("Yunior")) {
+            return "La persona introducida pudiera ser Yunior. Ya que contiene el mismo nombre.";
+        } else if (user.correo === "yunior.developer@hotmail.com"){
+            return "La persona introducida pudiera ser Yunior. Ya que tiene el mismo correo.";
+        } else if (parseInt(user.edad) === 27){
+            return "La persona introducida pudiera ser Yunior. Ya que tiene la misma edad.";
+        }
+    } else { 
+        return "La persona introducida no es Yunior";
+    }
 }
 
 
@@ -81,7 +113,53 @@ function ejercicio05(user){
 
 
 function ejercicio06(user){
-    console.log(user);
+    var regExName = new RegExp('[a-zA-Z ]{5,60}', 'g');
+    var arrayEmail = user.correo.split("@");
+    var nameIsCorrect = false;
+    var isValid = {
+        valid : false,
+        errors : [
+            {
+                code : 'email_invalid_min_length',
+                text : 'El correo [correo] debe contener más de 7 caracteres.'
+            }
+        ]
+    }
+
+    if (
+        regExName.test(user.nombre) && arrayEmail[0] !== "" && arrayEmail[1] !== "" 
+        && user.correo.length > 7 && user.correo.length < 60 
+        && user.edad > 5 && user.edad < 150
+    ) {
+
+        isValid.valid = true;
+        isValid.errors[0].code = "";
+        isValid.errors[0].text = "";
+
+    } else if(regExName.test(user.nombre)){
+
+        isValid.errors[0].code = 'name_invalid';
+        isValid.errors[0].text = `El nombre ${user.nombre} dete tener entre 5 y 60 caracteres y solo puede contener letras o espacios`;
+
+    } else if(arrayEmail[0] === "" || arrayEmail[1] === "" ) {
+
+        isValid.errors[0].code = 'email_format_invalid';
+        isValid.errors[0].text = `Email ${user.correo} con formato invalido, debe tener caracteres antes y despues del @`;
+
+    } else if(user.correo.length < 7 || user.correo.length > 60){
+
+        isValid.errors[0].code = 'email_invalid_min_length';
+        isValid.errors[0].text = `El correo ${user.correo} debe contener más de 7 caracteres.`;
+
+    } else if(user.edad < 5 || !user.edad > 150) {
+
+        isValid.errors[0].code = 'age_invalid';
+        isValid.errors[0].text = `La edad introducida no es correcta, ha de ser mayor de 5 y menor de 150`
+
+    }
+
+    return JSON.stringify(isValid);
+
 }
 
 
@@ -96,7 +174,37 @@ function ejercicio06(user){
 
 
 function ejercicio07(users){
-    console.log(users);
+
+    var objectInfo = {
+        items_inserted  : 0,
+        ids  : [],
+        with_errors  : [],
+        users_stored  : []
+    }
+
+    var usersValids = [];
+    users.forEach(user => {
+
+        var id = 0;
+        var isValid = JSON.parse(ejercicio06(user));
+
+        if(isValid.valid){
+
+            id = usersValids.length + 1;
+            user.id = id; 
+            usersValids.push(user);
+            objectInfo.items_inserted = id;
+            objectInfo.ids.push(id);
+            objectInfo.users_stored = usersValids;
+
+        } else{
+
+            objectInfo.with_errors.push(isValid.errors[0]);
+
+        }
+    });
+    
+    return JSON.stringify(objectInfo);
 }
 
 
